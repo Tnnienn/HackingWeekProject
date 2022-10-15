a6bd4503edc6d6d1fbfa497816ce360156b51781
Antonio Fasanella <fasanella.antonio.sanfele@gmail.com>
2022-10-15 20:59:33 +0200

    initial commit

---
+++ src/it/develhope/hackingweek/Position.java
@@ -0,0 +1,52 @@
+package it.develhope.hackingweek;
+
+import java.util.Objects;
+
+public class Position implements Comparable<Position>{
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public int compareTo(Position p) {
+        int xDiff = this.x - p.x;
+        if(xDiff != 0) {
+            return xDiff;
+        }
+        return this.y - p.y;
+    }
+}
---
+++ src/it/develhope/hackingweek/Test.java
@@ -0,0 +1,8 @@
+package it.develhope.hackingweek;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
